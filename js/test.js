992f46cbc8437f5a2618bd139885e862979de3f0
JD-PC\JD <josh.denton@student.anglia.ac.uk>
2020-09-30 21:52:27 +0100

    changeStyle Func

--- js/test.js
+++ js/test.js
@@ -2,12 +2,12 @@
 function checkPage() {
     if (document.URL.includes("PersonalProjects")) {
         var element = document.getElementById("PP");
-        element.style.fontWeight = "bold";
-        changeCol(element);
+        changeStyle(element);
     }
 }
 
-function changeCol(element) {
+function changeStyle(element) {
     element.style.color = "orange";
+    element.style.fontWeight = "bold";
 }
 window.onload = checkPage;
