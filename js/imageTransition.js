da7ed61addab3734d7d0427ce209cf638e91cc33
JD-PC\JD <josh.denton@student.anglia.ac.uk>
2020-10-02 19:53:25 +0100

    chnages image source

--- js/imageTransition.js
+++ js/imageTransition.js
@@ -2,10 +2,10 @@ var i = 0;
 var images = [];
 var time = 3000;
 
-images[0] = '/images/tf_1.jpg';
-images[1] = '/images/track.jpg';
-images[2] = '/images/bd_2.jpg';
-images[3] = '/images/ani_1.jpg';
+images[0] = './images/tf_1.jpg';
+images[1] = './images/track.jpg';
+images[2] = './images/bd_2.jpg';
+images[3] = './images/ani_1.jpg';
 
 function changeImg() {
     document.slide.src = images[i] + new Date().getTime();
