9c3a1dfe3cca7d502b63b544dff7649f9859afc1
JD-PC\JD <josh.denton@student.anglia.ac.uk>
2020-10-02 19:52:10 +0100

    image changes..

--- js/checkPage.js
+++ js/checkPage.js
@@ -1,7 +1,11 @@
 
 function checkPage() {
     if (document.URL.includes("PersonalProjects")) {
-        var element = document.getElementById("PP");
+        var element = document.getElementById("Personal Projects");
+        changeStyle(element);
+    }
+    else if (document.URL.includes("CV")) {
+        var element = document.getElementById("CV");
         changeStyle(element);
     }
 }
--- js/imageTransition.js
+++ js/imageTransition.js
@@ -2,13 +2,13 @@ var i = 0;
 var images = [];
 var time = 3000;
 
-images[0] = './images/tf_1.jpg';
-images[1] = './images/track.jpg';
-images[2] = './images/bd_2.jpg';
-images[3] = './images/ani_1.jpg';
+images[0] = '/images/tf_1.jpg';
+images[1] = '/images/track.jpg';
+images[2] = '/images/bd_2.jpg';
+images[3] = '/images/ani_1.jpg';
 
 function changeImg() {
-	document.slide.src = images[i];
+    document.slide.src = images[i] + new Date().getTime();
 	if (i < images.length - 1) {
 		i++;
 	} else
