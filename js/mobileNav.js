9b3a9927cac05924c443590b3674bbd9784e3302
JD-PC\JD <josh.denton@student.anglia.ac.uk>
2020-12-16 12:55:32 +0000

    colour changes

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,9 +1,13 @@
 function ChangeNavState() {
+
     var e = document.getElementById("linksMenu");
-    if (e.style.display === "block") {
-        e.style = "none";
-    }
-    else {
-        e.style.display = "block";
-    }
+    var test = document.getElementById("linksCon");
+
+    var elements = [e, test];
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].style.display === "block")
+            elements[i].style.display = "none";
+        else
+            elements[i].style.display = "block";
+    }   
 }
