c970e94257d8a28a5e46a211f666b4c506ce17b0
JD-PC\JD <josh.denton@student.anglia.ac.uk>
2020-12-16 11:47:25 +0000

    cleaned checkPage.js - more readable code

--- js/checkPage.js
+++ js/checkPage.js
@@ -1,23 +1,27 @@
 function checkPage() {
 
     if (document.URL.includes("PersonalProjects")) {
-        var element = document.getElementById("Personal Projects");
-        changeStyle(element);
+        GetElements("Personal Projects");
     }
     else if (document.URL.includes("CV")) {
-        var element = document.getElementById("CV");
-        changeStyle(element);
+        GetElements("CV");
     }
     else if (document.URL.includes("Contact")) {
-        var element = document.getElementById("Contact");
-        changeStyle(element);
+        GetElements("Contact");
     }
     else {
-        var element = document.getElementById("Home");
-        changeStyle(element);
+        GetElements("Home");
     }
 }
-function changeStyle(element) {
-    element.style.color = "orange";
+
+function GetElements(s) {
+    var elements = document.getElementsByClassName(s);
+    changeStyle(elements);
+}
+
+function changeStyle(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color = "orange";
+    }
 }
 checkPage();
